87021fc30ccdf047153870f5b340e70a21a5c980
teacher-kiwi <66172542+teacher-kiwi@users.noreply.github.com>
2021-01-27 01:12:18 +0900

    Add files via upload

--- 09html(4)/numCheck.js
+++ 09html(4)/numCheck.js
@@ -1,7 +1,7 @@
 const check = document.getElementById('check');
 const guess = new URLSearchParams(window.location.search);
 const num = guess.get('guess');
-const randomNum = Math.floor(Math.random() * 10) + 1;
+const randomNum = Math.floor(Math.random() * 3) + 1;
 
 if(num == randomNum){
   check.innerHTML = '정답입니다!';
